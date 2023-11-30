d35340f5624ab95e26e8932f164acb349ac2aa68
SiennaChien <sienna.chien@gmail.com>
2023-11-30 03:04:32 -0500

    UI page 1, including restaurant choices with toggled visibility

--- UI/sample.py
+++ UI/sample.py
@@ -7,7 +7,14 @@ app = Flask(__name__)
 def index():
     today = datetime.date.today().isoformat()
     five_days_later = (datetime.date.today() + datetime.timedelta(days=5)).isoformat()
-    return render_template('index.html', min_date=today, max_date=five_days_later)
+    name = "Varsha's Restaurant"
+    duration = "1"
+    distance = "1"
+    budget = "$$"
+    hours = "9am-5pm"
+    address = "123 Main St, Boston, MA"
+    
+    return render_template('index.html', min_date=today, max_date=five_days_later, restName1 = name, restDuration1 = duration, restDistance1 = distance, restBudget1 = budget, restHours1 = hours, restAddress1 = address)
 
 if __name__ == '__main__':
     app.run(debug=True)
