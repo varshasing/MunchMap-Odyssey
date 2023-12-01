bdc5d8a52b80c31bbfbf5f93e046f4461848df08
Peter Zhao <42500800+pzhao123@users.noreply.github.com>
2023-11-30 22:15:09 -0500

    Added redirect to result.html

--- UI/sample.py
+++ UI/sample.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import datetime
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET','POST'])
 def index():
     today = datetime.date.today().isoformat()
     five_days_later = (datetime.date.today() + datetime.timedelta(days=5)).isoformat()
@@ -14,7 +14,14 @@ def index():
     hours = "9am-5pm"
     address = "123 Main St, Boston, MA"
     
+    if request.method == 'POST':
+        return redirect('/result')
+    
     return render_template('index.html', min_date=today, max_date=five_days_later, restName1 = name, restDuration1 = duration, restDistance1 = distance, restBudget1 = budget, restHours1 = hours, restAddress1 = address)
+
+@app.route('/result', methods=['GET', 'POST'])
+def result():
+    return render_template('result.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
