2d89258e3136ed52fdbed811068e0da88fc5e359
SiennaChien <sienna.chien@gmail.com>
2023-12-01 03:10:37 -0500

    UI Page 1, including displaying data from Yelp API, missing hours data

--- yelpapi.py
+++ yelpapi.py
@@ -1,6 +1,11 @@
 import requests
+from dotenv import load_dotenv
+import os
 
-def search_yelp(api_key, term, latitude, longitude, radius=400):
+load_dotenv()
+api_key = os.getenv("YELP_KEY")
+
+def search_yelp(api_key, search_term, userPrice, latitude, longitude, radius=400): # after I sort the data, I need to return the top 3 results as a list each time I call this function
     # Yelp API endpoint for business search
     endpoint = "https://api.yelp.com/v3/businesses/search"
 
@@ -9,12 +14,14 @@ def search_yelp(api_key, term, latitude, longitude, radius=400):
         "Authorization": f"Bearer {api_key}"
     }
 
+
     # Set up the parameters for the search
     params = {
-        "term": term,
+        "term": search_term,
         "latitude": latitude,
         "longitude": longitude,
         "radius": radius,
+        "price": "1,2,3,4"
         #"cuisine": cuisine
     }
 
@@ -26,29 +33,48 @@ def search_yelp(api_key, term, latitude, longitude, radius=400):
         # Parse the JSON response
         data = response.json()
 
-
-        # Print the business information
+        # need to sort the business data by rating
         for business in data["businesses"]:
-            print(f"distance: {business['distance']}");
-            print(f"categories: {business['categories']}");
+            #print(f"{business['name']} ({business['rating']} stars) - {business['location']['address1']}")
+            sorted_businesses = sorted(data["businesses"], key=lambda x: x["rating"], reverse=True)
+        
+        final_sorted = [] #empty list
+        for business in sorted_businesses:
+            if business["price"] == "$" and userPrice == 1:
+                #print(f"{business['name']} ({business['rating']} stars) - {business['location']['address1']} - {business['price']}")
+                final_sorted.append(business)
+            elif business["price"] == "$$" and userPrice == 2:
+                #print(f"{business['name']} ({business['rating']} stars) - {business['location']['address1']} - {business['price']}")
+                final_sorted.append(business)
+            elif business["price"] == "$$$" and userPrice == 3:
+                #print(f"{business['name']} ({business['rating']} stars) - {business['location']['address1']} - {business['price']}")
+                final_sorted.append(business)
+            elif business["price"] == "$$$$" and userPrice == 4:
+                #print(f"{business['name']} ({business['rating']} stars) - {business['location']['address1']} - {business['price']}")
+                final_sorted.append(business)
 
-            categories = [category['title'] for category in business['categories']]
-            print(f"categories: {', '.join(categories)}")
-            print(f"Name: {business['name']}")
-            #print(f"Rating: {business['rating']}")
-            #print(f"Address: {', '.join(business['location']['display_address'])}")
-            print("\n")
+        last_list = []
+        # i want to make another list, only containting the top 3 results price, rating, name, address, and coordinates
+        for business in final_sorted:
+            last_list.append({"name": business["name"], "rating": business["rating"], "price": business["price"], "address": business["location"]["address1"], "coordinates": business["coordinates"]})
+        for i in last_list[:3]:
+            print(i["price"])
+            print(type(i))
+
+
+        return last_list[:3]
     else:
         print(f"Error: {response.status_code}, {response.text}")
 
-if __name__ == "__main__":
-    # Replace 'YOUR_API_KEY' with your actual Yelp API key
-    api_key = 'riko09ZEG7R1wBgMqZbjv4uNtMHGBb-t1-2zFrGjAy7Ka2nRwVqD8t3-6GPJXMTfDJEiuQ0RlM24Qh6umi_rVm2Gs7szTULJDRYPfsBEtPYqo0if4YP1_-RLlb9eZXYx'
+# if __name__ == "__main__":
+#     # Replace 'YOUR_API_KEY' with your actual Yelp API key
+#     api_key = 'riko09ZEG7R1wBgMqZbjv4uNtMHGBb-t1-2zFrGjAy7Ka2nRwVqD8t3-6GPJXMTfDJEiuQ0RlM24Qh6umi_rVm2Gs7szTULJDRYPfsBEtPYqo0if4YP1_-RLlb9eZXYx'
 
-    # Define your search term and coordinates
-    search_term = "Italian"
-    latitude = 37.7749  # Replace with the desired latitude
-    longitude = -122.4194  # Replace with the desired longitude
+#     # Define your search term and coordinates
+#     search_term = "Italian" #Passed by front end
+#     latitude = 37.7749  #passed by google maps api
+#     longitude = -122.4194 #passed by google maps api
+#     userPrice = 2   #passed by front end
 
-    # Perform the Yelp API search
-    search_yelp(api_key, search_term, latitude, longitude)
+#     # this will need to be called AFTER user enters their search_term.
+#     search_yelp(api_key, search_term, userPrice, latitude, longitude,)
